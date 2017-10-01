774fad7ab3d4ae10e92e00ca498d0d58d24b5ff3
ALEX-XPS\Alex <whisker.ship@gmail.com>
2017-10-01 15:17:11 -0700

    n

--- Assets/NPCFollowPath.cs
+++ Assets/NPCFollowPath.cs
@@ -2,26 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum PathType { MoveDirection, JumpDirection }
 public class NPCFollowPath : GeneralMovementv0 {
     Rigidbody2D rigid;
     private void Awake() {
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    bool followPath = false;
-    private void FixedUpdate() {
-        if (followPath == true) {
+    [SerializeField]
+    PathType[] pathTypes;
+    int currentPath = 0;
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.O)) {
+            followPath = true;
+            StartCoroutine(SetRigidPath(pathTypes[currentPath]));
+        }
 
+        if (followPath == true && pathIsRunning == false && currentPath < pathTypes.Length -1) {
+            currentPath++;
+            Debug.Log("current path is " + currentPath);
+            StartCoroutine(SetRigidPath(pathTypes[currentPath]));
         }
     }
 
-    void SetRigidPath (PathType pathType) {
-        switch (pathType) {
-            case pathType:
-            //
+    bool followPath = false;
+    private void FixedUpdate() {
+        if (followPath == true) {
+            //SetRigidPath(pathTypes[0].pathMovement, pathTypes[0].transform.position);
+        }
+    }
+
+    [SerializeField]
+    float moveSpeed = 20;
+    bool pathIsRunning;
+    IEnumerator SetRigidPath (PathType pathType) {
+        pathIsRunning = true;
+
+        switch (pathType.pathMovement) {
+            case PathMovement.Move:
+
+            while (Vector2.Distance(transform.position, pathType.transform.position) > 2) {
+                Vector2 dir = (pathType.transform.position - transform.position).normalized;
+                rigid.velocity = dir * moveSpeed;
+
+                yield return null;
+            }
+
+            break;
+
+            case PathMovement.Jump:
+            Debug.Log("should be jumping");
+            rigid.velocity = new Vector2(pathType.jumpDirection, pathType.jumpForce);
+
             break;
         }
 
+        rigid.velocity = Vector2.zero;
+        Debug.Log("rigid path done");
+        pathIsRunning = false;
     }
 }
